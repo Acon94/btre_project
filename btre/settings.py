e92d07c1d8587be6ff8de3a6f549fcad1e47516b
Andrew Connolly <andrew@Andrews-MacBook-Pro.local>
2020-04-07 18:27:19 +0100

    added steeings.py updates

--- btre/settings.py
+++ btre/settings.py
@@ -145,13 +145,6 @@ Message_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# email config
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='achammers0@gmail.com'
-EMAIL_HOST_PASSWORD='Oldhead@12'
-EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
